2879a5381dc7c8301cc607039c753e3238fece2f
StevenPhang22 <sphang@ufl.edu>
2023-01-29 01:01:43 -0500

    integration 1

--- application/app.py
+++ application/app.py
@@ -1,11 +1,16 @@
 import kivy
+import sttDriver
+import queue
+from threading import Thread
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.list import TwoLineAvatarIconListItem, IconLeftWidget
 
 prompt = 'What type of interview would you like to prepare for?'
 CGPT = 'ChatGPT'
+response_q = queue.Queue()
 
 class StartScreen(Screen):
     pass
@@ -18,6 +23,11 @@ class MainApp(MDApp):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
         sm = Builder.load_file('app.kv')
+
+        tts_thread = Thread(target=sttDriver.stt_driver_main, args=(response_q,))
+        tts_thread.start()
+
+        Clock.schedule_interval(self.my_callback, 1 / 30.)
         return sm
 
     def session(self, name_in, api_key_in):
@@ -35,7 +45,7 @@ class MainApp(MDApp):
 
     def load_main(self):
         if len(self.root.ids.main_screen.ids.chatlist.children) == 0:
-            file = open("./application/storage/transcript.txt", 'w')
+            file = open("./storage/transcript.txt", 'w')
             file.write(CGPT + ': ' + prompt)
             self.add_msg(CGPT, prompt)
             self.add_msg(name, 'temp')
@@ -59,6 +69,14 @@ class MainApp(MDApp):
             radius=radius
         )
         self.root.ids.main_screen.ids.chatlist.add_widget(widget)
+
+    def my_callback(self, soup):
+        if not response_q.empty():
+            response = response_q.get()
+            if response[1]:
+                self.add_msg(self.name, response[0])
+            else:
+                self.add_msg(CGPT, response[0])
 
 if __name__ == "__main__":
     MainApp().run()
--- sttDriver.py
+++ sttDriver.py
@@ -1,21 +1,23 @@
 import speechToText
-import queue
 import sys
 import ChatGPT
+import queue
 import os
+import application.app as app
 from io import BytesIO
 from playsound import playsound
 from gtts import gTTS
 from threading import Thread
 from pydub import AudioSegment
 from pydub.playback import play
+import application.app as app
 
 RED = "\033[0;31m"
 GREEN = "\033[0;32m"
 YELLOW = "\033[0;33m"
 
 
-def main():
+def stt_driver_main(response_q):
     transcription_q = queue.Queue()
     thread = Thread(target=speechToText.start_speech_to_text, args=(transcription_q,))
     thread.start()
@@ -25,14 +27,18 @@ def main():
     """)
 
     transaction = 0
+
     while True:
         message = transcription_q.get()
         if message[1] == 1:
             sys.stdout.write(GREEN)
             sys.stdout.write("\r" + message[0] + "\n")
 
+            response_q.put([message[0], 1])
             response = chat_gpt3.get_prompt(message[0], transaction > 3)
             print(response)
+            response_q.put([response, 0])
+
             mp3_fp = BytesIO()
             tts = gTTS(text=response, lang='en')
             tts.write_to_fp(mp3_fp)
@@ -50,5 +56,22 @@ def main():
             sys.stdout.write(RED)
             sys.stdout.write("\r" + message[0])
 
-if __name__ == "__main__":
-    main()
+
+# rbq = RankByQualifier("Strong problem-solving and analytical skills")
+# rbq = RankByQualifier("motivated")
+# rbq = RankByQualifier("confident and commanding")
+#rbq = RankByQualifier("positive")
+#print("Bad: " + rbq.get_prompt("I was unfortunately unable to meet the deadline. My VHDL code also never worked."))
+#print("Good: " + rbq.get_prompt("I was unfortunately unable to meet the deadline. Despite this, I worked with my managers to develop a plan to recover from this and was able to meet the second milestone in advance of the deadline!"))
+
+
+#chat_gpt3 = ChatGPT3(initial_context="""You are a senior computer engineer interviewing me for a position at your company, Google. You will ask several questions and I will respond to those questions until you initiate the end of the interview after four or so questions. The only exception to this will be during the intrpductions at the start of the mock interview where I introduce myself first
+
+#""")
+
+#for i in range(6):
+#    user_input = input()
+#    if user_input == "quit":
+#        break
+#    print(chat_gpt3.get_prompt(user_input, i > 3))
+
